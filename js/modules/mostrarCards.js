b96341659f5937d9a8d4cd8957198bdc0a2e2b52
SilasLeao <leaosilas@gmail.com>
2023-10-17 17:59:51 -0300

    Update mostrarCards.js

--- js/modules/mostrarCards.js
+++ js/modules/mostrarCards.js
@@ -6,20 +6,22 @@ export function mostrarCards() {
     const existingData = JSON.parse(localStorage.getItem('crimeCards')) || [];
 
     for(const cardData of existingData) {
+        
         const { tipo, data, descricao } = cardData
         const crimeCard = createCrimeCard(tipo, data, descricao);
         const deleteBtn = document.createElement('span');
+        
         deleteBtn.classList.add('material-symbols-outlined')
         deleteBtn.classList.add('deleteBtn')
         deleteBtn.innerHTML = 'close'
         deleteBtn.addEventListener('click', () => {
-        cardContainer.removeChild(crimeCard)
-        const cardIndex = existingData.indexOf(cardData);
-        if (cardIndex !== -1) {
-            existingData.splice(cardIndex, 1);
-            localStorage.setItem('crimeCards', JSON.stringify(existingData));
-        }
-        })
+            cardContainer.removeChild(crimeCard)
+            const cardIndex = existingData.indexOf(cardData);
+            if (cardIndex !== -1) {
+                existingData.splice(cardIndex, 1);
+                localStorage.setItem('crimeCards', JSON.stringify(existingData));
+            }
+            })
         crimeCard.firstChild.append(deleteBtn)
         cardContainer.append(crimeCard)       
     }
